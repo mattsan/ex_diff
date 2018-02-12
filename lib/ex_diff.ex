8db73c19288d43b9f7d7b4c7e95c22cb96d7f77d
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-02-12 09:54:42 +0900

    initial commit

---
+++ lib/ex_diff.ex
@@ -0,0 +1,18 @@
+defmodule ExDiff do
+  @moduledoc """
+  Documentation for ExDiff.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExDiff.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
